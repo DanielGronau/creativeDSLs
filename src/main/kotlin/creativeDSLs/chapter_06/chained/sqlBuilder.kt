0609f7dd2568d4e03694702aad6599c67dc3485f
DanielGronau <Daniel.Gronau@gmx.de>
2022-09-06 21:37:37 +0200

    chained builder SQL

---
+++ src/main/kotlin/creativeDSLs/chapter_06/chained/sqlBuilder.kt
@@ -0,0 +1,72 @@
+package creativeDSLs.chapter_06.chained
+
+object SqlQuery {
+    fun select(vararg columns: String) = SelectClause(*columns)
+}
+
+class SelectClause(vararg val columns: String) {
+    fun from(tableName: String) = FromClause(columns.asList(), tableName to null)
+    fun from(tableName: String, alias: String) = FromClause(columns.asList(), tableName to alias)
+}
+
+typealias NameWithAlias = Pair<String, String?>
+
+data class FromClause(
+    val columns: List<String>,
+    val tableName: NameWithAlias,
+    val joinClauses: List<Triple<NameWithAlias, String, String>> = listOf()
+) {
+    fun join(tableName: String) = JoinClause(this, tableName to null)
+    fun join(tableName: String, alias: String) = JoinClause(this, tableName to alias)
+
+    fun where(condition: String) = WhereClause(columns, tableName, joinClauses, listOf(condition))
+
+    fun build() = build(columns, tableName, joinClauses, listOf())
+}
+
+data class JoinClause(val fromClause: FromClause, val tableName: NameWithAlias) {
+    fun on(firstColumn: String, secondColumn: String) =
+        fromClause.copy(joinClauses = fromClause.joinClauses + Triple(tableName, firstColumn, secondColumn))
+}
+
+data class WhereClause(
+    val columns: List<String>,
+    val tableName: NameWithAlias,
+    val joinClauses: List<Triple<NameWithAlias, String, String>>,
+    val conditions: List<String>
+) {
+    fun and(condition: String) = copy(conditions = conditions + condition)
+    fun build() = build(columns, tableName, joinClauses, conditions)
+}
+
+private fun build(
+    columns: List<String>,
+    tableName: NameWithAlias,
+    joinClauses: List<Triple<NameWithAlias, String, String>>,
+    conditions: List<String>
+): String {
+    val sb = StringBuilder()
+        .append("SELECT ${columns.joinToString(", ") { it }}")
+        .append("\nFROM ")
+        .append(nameWithAlias(tableName))
+    joinClauses.forEach { (n, c1, c2) -> sb.append("\nJOIN ${nameWithAlias(n)} ON $c1 = $c2") }
+    if (conditions.isNotEmpty()) {
+        sb.append("\nWHERE ${conditions.joinToString("\nAND ")}")
+    }
+    return sb.toString()
+}
+
+private fun nameWithAlias(name: NameWithAlias) = when (name.second) {
+    null -> name.first
+    else -> "${name.first} AS ${name.second}"
+}
+
+fun main() {
+    val query = SqlQuery
+        .select("p.firstName", "p.lastName", "p.income")
+        .from("Person", "p")
+        .join("Address", "a").on("p.addressId","a.id")
+        .where("p.age > 18")
+        .and("a.city = 'London'")
+    println(query.build())
+}
