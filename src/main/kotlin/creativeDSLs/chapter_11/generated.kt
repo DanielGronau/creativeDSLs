21c843dfe59534f74f3f74aff2c4ebda5cfcdeb2
DanielGronau <Daniel.Gronau@gmx.de>
2022-09-12 00:35:37 +0200

    chapter 11

---
+++ src/main/kotlin/creativeDSLs/chapter_11/generated.kt
@@ -0,0 +1,342 @@
+package creativeDSLs.chapter_11
+
+import kotlin.Double
+
+public val Double.s: Second
+  get() = Second(this * 1.0)
+
+public val Double.min: Second
+  get() = Second(this * 60.0)
+
+public val Double.hrs: Second
+  get() = Second(this * 3600.0)
+
+public val Double.yr: Second
+  get() = Second(this * 3.1556925216E7)
+
+public val Double.mm: Meter
+  get() = Meter(this * 0.001)
+
+public val Double.cm: Meter
+  get() = Meter(this * 0.01)
+
+public val Double.`in`: Meter
+  get() = Meter(this * 0.0254)
+
+public val Double.ft: Meter
+  get() = Meter(this * 0.3048)
+
+public val Double.yd: Meter
+  get() = Meter(this * 0.9144)
+
+public val Double.m: Meter
+  get() = Meter(this * 1.0)
+
+public val Double.km: Meter
+  get() = Meter(this * 1000.0)
+
+public val Double.mi: Meter
+  get() = Meter(this * 1609.344)
+
+public val Double.mg: Kilogram
+  get() = Kilogram(this * 1.0E-6)
+
+public val Double.g: Kilogram
+  get() = Kilogram(this * 0.001)
+
+public val Double.kg: Kilogram
+  get() = Kilogram(this * 1.0)
+
+public val Double.tons: Kilogram
+  get() = Kilogram(this * 1000.0)
+
+public val Double.mm2: SquareMeter
+  get() = SquareMeter(this * 1.0E-6)
+
+public val Double.m2: SquareMeter
+  get() = SquareMeter(this * 1.0)
+
+public val Double.km2: SquareMeter
+  get() = SquareMeter(this * 1000000.0)
+
+public val Double.mm3: CubicMeter
+  get() = CubicMeter(this * 1.0E-9)
+
+public val Double.l: CubicMeter
+  get() = CubicMeter(this * 0.001)
+
+public val Double.m3: CubicMeter
+  get() = CubicMeter(this * 1.0)
+
+public val Double.km3: CubicMeter
+  get() = CubicMeter(this * 1.0E9)
+
+public val Double.m_s: MeterPerSecond
+  get() = MeterPerSecond(this * 1.0)
+
+public val Double.km_h: MeterPerSecond
+  get() = MeterPerSecond(this * 0.2777777777777778)
+
+public val Double.m_s2: MeterPerSecondSquared
+  get() = MeterPerSecondSquared(this * 1.0)
+
+public val Double.N: Newton
+  get() = Newton(this * 1.0)
+
+public val Double.kN: Newton
+  get() = Newton(this * 1000.0)
+
+public val Double.mJ: Joule
+  get() = Joule(this * 1.0E-4)
+
+public val Double.J: Joule
+  get() = Joule(this * 1.0)
+
+public val Double.kJ: Joule
+  get() = Joule(this * 1000.0)
+
+public val Double.MegaJ: Joule
+  get() = Joule(this * 1000000.0)
+
+public val Double.mW: Watt
+  get() = Watt(this * 0.001)
+
+public val Double.W: Watt
+  get() = Watt(this * 1.0)
+
+public val Double.kW: Watt
+  get() = Watt(this * 1000.0)
+
+public val Double.MegaW: Watt
+  get() = Watt(this * 1000000.0)
+
+public val Double.mP: Pascal
+  get() = Pascal(this * 0.001)
+
+public val Double.P: Pascal
+  get() = Pascal(this * 1.0)
+
+public val Double.hP: Pascal
+  get() = Pascal(this * 100.0)
+
+public val Double.kP: Pascal
+  get() = Pascal(this * 1000.0)
+
+public val Double.MegaP: Pascal
+  get() = Pascal(this * 1000000.0)
+
+public val Second.s: Double
+  get() = this.amount / 1.0
+
+public val Second.min: Double
+  get() = this.amount / 60.0
+
+public val Second.hrs: Double
+  get() = this.amount / 3600.0
+
+public val Second.yr: Double
+  get() = this.amount / 3.1556925216E7
+
+public val Meter.mm: Double
+  get() = this.amount / 0.001
+
+public val Meter.cm: Double
+  get() = this.amount / 0.01
+
+public val Meter.`in`: Double
+  get() = this.amount / 0.0254
+
+public val Meter.ft: Double
+  get() = this.amount / 0.3048
+
+public val Meter.yd: Double
+  get() = this.amount / 0.9144
+
+public val Meter.m: Double
+  get() = this.amount / 1.0
+
+public val Meter.km: Double
+  get() = this.amount / 1000.0
+
+public val Meter.mi: Double
+  get() = this.amount / 1609.344
+
+public val Kilogram.mg: Double
+  get() = this.amount / 1.0E-6
+
+public val Kilogram.g: Double
+  get() = this.amount / 0.001
+
+public val Kilogram.kg: Double
+  get() = this.amount / 1.0
+
+public val Kilogram.tons: Double
+  get() = this.amount / 1000.0
+
+public val SquareMeter.mm2: Double
+  get() = this.amount / 1.0E-6
+
+public val SquareMeter.m2: Double
+  get() = this.amount / 1.0
+
+public val SquareMeter.km2: Double
+  get() = this.amount / 1000000.0
+
+public val CubicMeter.mm3: Double
+  get() = this.amount / 1.0E-9
+
+public val CubicMeter.l: Double
+  get() = this.amount / 0.001
+
+public val CubicMeter.m3: Double
+  get() = this.amount / 1.0
+
+public val CubicMeter.km3: Double
+  get() = this.amount / 1.0E9
+
+public val MeterPerSecond.m_s: Double
+  get() = this.amount / 1.0
+
+public val MeterPerSecond.km_h: Double
+  get() = this.amount / 0.2777777777777778
+
+public val MeterPerSecondSquared.m_s2: Double
+  get() = this.amount / 1.0
+
+public val Newton.N: Double
+  get() = this.amount / 1.0
+
+public val Newton.kN: Double
+  get() = this.amount / 1000.0
+
+public val Joule.mJ: Double
+  get() = this.amount / 1.0E-4
+
+public val Joule.J: Double
+  get() = this.amount / 1.0
+
+public val Joule.kJ: Double
+  get() = this.amount / 1000.0
+
+public val Joule.MegaJ: Double
+  get() = this.amount / 1000000.0
+
+public val Watt.mW: Double
+  get() = this.amount / 0.001
+
+public val Watt.W: Double
+  get() = this.amount / 1.0
+
+public val Watt.kW: Double
+  get() = this.amount / 1000.0
+
+public val Watt.MegaW: Double
+  get() = this.amount / 1000000.0
+
+public val Pascal.mP: Double
+  get() = this.amount / 0.001
+
+public val Pascal.P: Double
+  get() = this.amount / 1.0
+
+public val Pascal.hP: Double
+  get() = this.amount / 100.0
+
+public val Pascal.kP: Double
+  get() = this.amount / 1000.0
+
+public val Pascal.MegaP: Double
+  get() = this.amount / 1000000.0
+
+public operator fun CubicMeter.plus(that: CubicMeter) = copy(amount = this.amount + that.amount)
+
+public operator fun Joule.plus(that: Joule) = copy(amount = this.amount + that.amount)
+
+public operator fun Kilogram.plus(that: Kilogram) = copy(amount = this.amount + that.amount)
+
+public operator fun Meter.plus(that: Meter) = copy(amount = this.amount + that.amount)
+
+public operator fun MeterPerSecond.plus(that: MeterPerSecond) = copy(amount = this.amount +
+    that.amount)
+
+public operator fun MeterPerSecondSquared.plus(that: MeterPerSecondSquared) = copy(amount =
+    this.amount + that.amount)
+
+public operator fun Newton.plus(that: Newton) = copy(amount = this.amount + that.amount)
+
+public operator fun Pascal.plus(that: Pascal) = copy(amount = this.amount + that.amount)
+
+public operator fun Second.plus(that: Second) = copy(amount = this.amount + that.amount)
+
+public operator fun SquareMeter.plus(that: SquareMeter) = copy(amount = this.amount + that.amount)
+
+public operator fun Watt.plus(that: Watt) = copy(amount = this.amount + that.amount)
+
+public operator fun CubicMeter.minus(that: CubicMeter) = copy(amount = this.amount - that.amount)
+
+public operator fun Joule.minus(that: Joule) = copy(amount = this.amount - that.amount)
+
+public operator fun Kilogram.minus(that: Kilogram) = copy(amount = this.amount - that.amount)
+
+public operator fun Meter.minus(that: Meter) = copy(amount = this.amount - that.amount)
+
+public operator fun MeterPerSecond.minus(that: MeterPerSecond) = copy(amount = this.amount -
+    that.amount)
+
+public operator fun MeterPerSecondSquared.minus(that: MeterPerSecondSquared) = copy(amount =
+    this.amount - that.amount)
+
+public operator fun Newton.minus(that: Newton) = copy(amount = this.amount - that.amount)
+
+public operator fun Pascal.minus(that: Pascal) = copy(amount = this.amount - that.amount)
+
+public operator fun Second.minus(that: Second) = copy(amount = this.amount - that.amount)
+
+public operator fun SquareMeter.minus(that: SquareMeter) = copy(amount = this.amount - that.amount)
+
+public operator fun Watt.minus(that: Watt) = copy(amount = this.amount - that.amount)
+
+public operator fun CubicMeter.unaryMinus() = copy(amount = -this.amount)
+
+public operator fun Joule.unaryMinus() = copy(amount = -this.amount)
+
+public operator fun Kilogram.unaryMinus() = copy(amount = -this.amount)
+
+public operator fun Meter.unaryMinus() = copy(amount = -this.amount)
+
+public operator fun MeterPerSecond.unaryMinus() = copy(amount = -this.amount)
+
+public operator fun MeterPerSecondSquared.unaryMinus() = copy(amount = -this.amount)
+
+public operator fun Newton.unaryMinus() = copy(amount = -this.amount)
+
+public operator fun Pascal.unaryMinus() = copy(amount = -this.amount)
+
+public operator fun Second.unaryMinus() = copy(amount = -this.amount)
+
+public operator fun SquareMeter.unaryMinus() = copy(amount = -this.amount)
+
+public operator fun Watt.unaryMinus() = copy(amount = -this.amount)
+
+public operator fun Double.times(that: CubicMeter) = that.copy(amount = this * that.amount)
+
+public operator fun Double.times(that: Joule) = that.copy(amount = this * that.amount)
+
+public operator fun Double.times(that: Kilogram) = that.copy(amount = this * that.amount)
+
+public operator fun Double.times(that: Meter) = that.copy(amount = this * that.amount)
+
+public operator fun Double.times(that: MeterPerSecond) = that.copy(amount = this * that.amount)
+
+public operator fun Double.times(that: MeterPerSecondSquared) = that.copy(amount = this *
+    that.amount)
+
+public operator fun Double.times(that: Newton) = that.copy(amount = this * that.amount)
+
+public operator fun Double.times(that: Pascal) = that.copy(amount = this * that.amount)
+
+public operator fun Double.times(that: Second) = that.copy(amount = this * that.amount)
+
+public operator fun Double.times(that: SquareMeter) = that.copy(amount = this * that.amount)
+
+public operator fun Double.times(that: Watt) = that.copy(amount = this * that.amount)
---
+++ src/main/kotlin/creativeDSLs/chapter_11/units.kt
@@ -0,0 +1,170 @@
+package creativeDSLs.chapter_11
+
+import com.squareup.kotlinpoet.FileSpec
+import com.squareup.kotlinpoet.FunSpec
+import com.squareup.kotlinpoet.KModifier
+import com.squareup.kotlinpoet.PropertySpec
+import java.nio.file.Path
+import kotlin.reflect.KClass
+
+sealed interface Amount {
+    val amount: Double
+}
+
+// base units
+data class Second(override val amount: Double) : Amount
+data class Meter(override val amount: Double) : Amount
+data class Kilogram(override val amount: Double) : Amount
+
+// derived units
+data class SquareMeter(override val amount: Double) : Amount
+data class CubicMeter(override val amount: Double) : Amount
+data class MeterPerSecond(override val amount: Double) : Amount
+data class MeterPerSecondSquared(override val amount: Double) : Amount
+data class Newton(override val amount: Double) : Amount
+data class Joule(override val amount: Double) : Amount
+data class Watt(override val amount: Double) : Amount
+data class Pascal(override val amount: Double) : Amount
+
+private val fromToDouble = listOf(
+    Triple("s", Second::class, 1.0),
+    Triple("min", Second::class, 60.0),
+    Triple("hrs", Second::class, 3600.0),
+    Triple("yr", Second::class, 31_556_925.216),
+
+    Triple("mm", Meter::class, 0.001),
+    Triple("cm", Meter::class, 0.01),
+    Triple("in", Meter::class, 0.0254),
+    Triple("ft", Meter::class, 0.3048),
+    Triple("yd", Meter::class, 0.9144),
+    Triple("m", Meter::class, 1.0),
+    Triple("km", Meter::class, 1000.0),
+    Triple("mi", Meter::class, 1609.344),
+
+    Triple("mg", Kilogram::class, 0.000_001),
+    Triple("g", Kilogram::class, 0.001),
+    Triple("kg", Kilogram::class, 1.0),
+    Triple("tons", Kilogram::class, 1000.0),
+
+    Triple("mm2", SquareMeter::class, 0.000_001),
+    Triple("m2", SquareMeter::class, 1.0),
+    Triple("km2", SquareMeter::class, 1_000_000.0),
+
+    Triple("mm3", CubicMeter::class, 0.000_000_001),
+    Triple("l", CubicMeter::class, 0.001),
+    Triple("m3", CubicMeter::class, 1.0),
+    Triple("km3", CubicMeter::class, 1_000_000_000.0),
+
+    Triple("m_s", MeterPerSecond::class, 1.0),
+    Triple("km_h", MeterPerSecond::class, 0.2777777777777778),
+
+    Triple("m_s2", MeterPerSecondSquared::class, 1.0),
+
+    Triple("N", Newton::class, 1.0),
+    Triple("kN", Newton::class, 1000.0),
+
+    Triple("mJ", Joule::class, 0.0001),
+    Triple("J", Joule::class, 1.0),
+    Triple("kJ", Joule::class, 1000.0),
+    Triple("MegaJ", Joule::class, 1_000_000.0),
+
+    Triple("mW", Watt::class, 0.001),
+    Triple("W", Watt::class, 1.0),
+    Triple("kW", Watt::class, 1000.0),
+    Triple("MegaW", Watt::class, 1_000_000.0),
+
+    Triple("mP", Pascal::class, 0.001),
+    Triple("P", Pascal::class, 1.0),
+    Triple("hP", Pascal::class, 100.0),
+    Triple("kP", Pascal::class, 1000.0),
+    Triple("MegaP", Pascal::class, 1_000_000.0),
+)
+
+private fun makeDoubleToAmount(unit: String, kClass: KClass<out Amount>, factor: Double) =
+    PropertySpec.builder(unit, kClass)
+        .receiver(Double::class)
+        .getter(
+            FunSpec.getterBuilder()
+                .addStatement("return %T(this * %L)", kClass, factor)
+                .build()
+        )
+        .build()
+
+private fun makeAmountToDouble(unit: String, kClass: KClass<out Amount>, factor: Double) =
+    PropertySpec.builder(unit, Double::class)
+        .receiver(kClass)
+        .getter(
+            FunSpec.getterBuilder()
+                .addStatement("return this.amount / %L", factor)
+                .build()
+        )
+        .build()
+
+private fun makeAddition(kClass: KClass<out Amount>) =
+    FunSpec.builder("plus")
+        .addModifiers(KModifier.OPERATOR)
+        .receiver(kClass)
+        .addParameter("that", kClass)
+        .addStatement("return copy(amount = this.amount + that.amount)")
+        .build()
+
+private fun makeSubtraction(kClass: KClass<out Amount>) =
+    FunSpec.builder("minus")
+        .addModifiers(KModifier.OPERATOR)
+        .receiver(kClass)
+        .addParameter("that", kClass)
+        .addStatement("return copy(amount = this.amount - that.amount)")
+        .build()
+
+private fun makeNegation(kClass: KClass<out Amount>) =
+    FunSpec.builder("unaryMinus")
+        .addModifiers(KModifier.OPERATOR)
+        .receiver(kClass)
+        .addStatement("return copy(amount = -this.amount)")
+        .build()
+
+private fun makeScalarMultiplication(kClass: KClass<out Amount>) =
+    FunSpec.builder("times")
+        .addModifiers(KModifier.OPERATOR)
+        .receiver(Double::class)
+        .addParameter("that", kClass)
+        .addStatement("return that.copy(amount = this * that.amount)")
+        .build()
+
+private fun makeDoubleToAmounts() =
+    fromToDouble.map { (u, k, f) -> makeDoubleToAmount(u, k, f) }
+
+private fun makeAmountToDoubles() =
+    fromToDouble.map { (u, k, f) -> makeAmountToDouble(u, k, f) }
+
+private fun makeAdditions() =
+    Amount::class.sealedSubclasses.map { makeAddition(it) }
+
+private fun makeSubtractions() =
+    Amount::class.sealedSubclasses.map { makeSubtraction(it) }
+
+private fun makeNegations() =
+    Amount::class.sealedSubclasses.map { makeNegation(it) }
+
+private fun makeScalarMultiplications() =
+    Amount::class.sealedSubclasses.map { makeScalarMultiplication(it) }
+
+fun main() {
+    val builder = FileSpec.builder("creativeDSLs.chapter_11", "generated")
+
+    makeDoubleToAmounts().forEach { builder.addProperty(it) }
+
+    makeAmountToDoubles().forEach { builder.addProperty(it) }
+
+    makeAdditions().forEach { builder.addFunction(it) }
+
+    makeSubtractions().forEach { builder.addFunction(it) }
+
+    makeNegations().forEach { builder.addFunction(it)}
+
+    makeScalarMultiplications().forEach { builder.addFunction(it)}
+
+    builder.build()
+        .writeTo(Path.of("./src/main/kotlin/"))
+}
+
