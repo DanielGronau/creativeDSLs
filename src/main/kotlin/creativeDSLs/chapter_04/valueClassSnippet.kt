6ad11b0029ac8a888753ce85038cbdb12e0eaccf
DanielGronau <daniel.gronau@gmx.de>
2023-07-05 00:24:42 +0200

    chapter-04 extended value class example, added conclusion

--- src/main/kotlin/creativeDSLs/chapter_04/valueClassSnippet.kt
+++ src/main/kotlin/creativeDSLs/chapter_04/valueClassSnippet.kt
@@ -1,9 +1,16 @@
 package creativeDSLs.chapter_04
 
-import java.math.BigDecimal
-
 @JvmInline
 value class Kilometers(val value: Double)
 
 @JvmInline
 value class Miles(val value: Double)
+
+fun Kilometers.toMiles() : Miles = Miles(this.value * 0.6214)
+
+val marathonMiles = Kilometers(42.195).toMiles() // Miles(value=26.219973)
+
+fun main() {
+    println(marathonMiles)
+}
+
