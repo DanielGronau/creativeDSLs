c6c7738c1c6e8253cd7cb672d4b402e0dce7731c
DanielGronau <daniel.gronau@gmx.de>
2023-11-17 09:53:41 +0100

    mapper improvement

--- src/main/kotlin/creativeDSLs/chapter_10/mapper.kt
+++ src/main/kotlin/creativeDSLs/chapter_10/mapper.kt
@@ -4,6 +4,7 @@ import java.time.ZonedDateTime
 import java.time.temporal.ChronoUnit
 import java.util.*
 import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
 import kotlin.reflect.full.*
 
 @Repeatable
@@ -12,43 +13,55 @@ import kotlin.reflect.full.*
 annotation class Mapping(
     val source: String,
     val target: String,
-    val transformer: KClass<*> = Nothing::class
+    val transformer: KClass<*> = Nothing::class,
 )
 
 abstract class Mapper<S : Any, T : Any> {
     fun map(s: S): T {
         val annotations = this::class.findAnnotations(Mapping::class)
-        val targetType = this::class.supertypes[0].arguments[1].type!!.classifier as KClass<*>
+        val targetType = this::class
+            .supertypes[0]
+            .arguments[1]
+            .type!!
+            .classifier as KClass<*>
         val targetConstructor = targetType.primaryConstructor!!
         val args = targetConstructor.parameters.map { targetParam ->
             val ann = annotations.find { it.target == targetParam.name }
             val sourceParam = ann?.source ?: targetParam.name
-            val sourceValue = s::class.memberProperties.find { it.name == sourceParam }!!.getter.call(s)
-            val hasTransformer = ann?.transformer?.isSubclassOf(Function1::class) ?: false
+            val sourceValue = s::class.memberProperties
+                .find { it.name == sourceParam }!!
+                .getter.call(s)
             when {
-                hasTransformer -> {
-                    val transformer = ann!!.transformer.objectInstance ?: ann.transformer.primaryConstructor!!.call()
+                ann?.transformer != null -> {
+                    val transformer = ann.transformer.objectInstance
+                        ?: ann.transformer.primaryConstructor?.call()
+                        ?: Unit
                     transformer::class.memberFunctions
-                        .find { it.name == "invoke" }!!
-                        .call(transformer, sourceValue)
+                        .find { it.name == "invoke" }
+                        ?.call(transformer, sourceValue)
+                        ?: sourceValue
                 }
+
                 else -> sourceValue
             }
         }.toTypedArray()
         println(args.toList())
+        @Suppress("UNCHECKED_CAST")
         return targetConstructor.call(*args) as T
     }
 }
-
 
 data class User(val id: UUID, val firstName: String, val familyName: String, val birthDay: ZonedDateTime)
 
 data class Person(val firstName: String, val lastName: String, val age: Int)
 
-object AgeTransformer : (ZonedDateTime) -> Int {
-    override fun invoke(z: ZonedDateTime) =
-        ChronoUnit.YEARS.between(z, ZonedDateTime.now()).toInt()
+open class Transformer<in A, out B>(val lambda: (A) -> B) : (A) -> B {
+    override fun invoke(a: A): B = lambda(a)
 }
+
+object AgeTransformer : Transformer<ZonedDateTime, Int>({ z ->
+    ChronoUnit.YEARS.between(z, ZonedDateTime.now()).toInt()
+})
 
 @Mapping("familyName", "lastName")
 @Mapping("birthDay", "age", AgeTransformer::class)
