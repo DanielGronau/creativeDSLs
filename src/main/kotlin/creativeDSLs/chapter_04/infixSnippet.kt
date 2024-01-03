99fa46ddca26b119d08ae2e3f341fe82a2999db1
DanielGronau <daniel.gronau@gmx.de>
2024-01-03 21:53:36 +0100

    chapter 04 infix with trailing lambda example

--- src/main/kotlin/creativeDSLs/chapter_04/infixSnippet.kt
+++ src/main/kotlin/creativeDSLs/chapter_04/infixSnippet.kt
@@ -15,3 +15,11 @@ infix fun Double.`^`(exponent: Double) = this.pow(exponent)
 infix fun Double.`^`(exponent: Int) = this.pow(exponent)
 
 val x = 1.2 `^` 3
+
+infix fun <P, Q, R> Pair<List<P>, List<Q>>.zipWith(body: (P, Q) -> R): List<R> =
+    first.zip(second).map { (p, q) -> body(p, q) }
+
+fun main() {
+    val l = listOf(1, 2, 3, 4) to listOf("x", "y", "z") zipWith { i, c -> "$i -> $c" }
+    println(l)
+}
